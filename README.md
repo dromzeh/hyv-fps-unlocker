390cfe7fa960c097d114270cb350dbf4c565d333
Marcel <65048232+dromzeh@users.noreply.github.com>
2023-02-26 22:02:52 +0000

    feat: add initial project files
    - add main code `index.js`
    - add `readme` for documentation
    - add `package.json` and `pnpm-lock.yaml` (deps)

---
+++ README.md
@@ -0,0 +1,46 @@
+# hi3-fps-unlock
+
+Program that allows you to modify the fps of Hi3 in and outside of combat to any value of your choice.
+- This is done by registry editing using the `winreg` library.
+
+*Disclaimer: I am not responsible for any damage caused by this program. Use at your own risk.*
+
+<p align = "center">
+    <a href="https://buymeacoffee.com/marcelmd" alt="buymeacoffee">
+        <img src="https://img.shields.io/badge/Buy_Me_A_Coffee-FFDD00?style=for-the-badge&logo=buy-me-a-coffee&logoColor=black"/></a>
+    <a href="https://nodejs.org" alt="node js">
+        <img src="https://img.shields.io/badge/Node.js-339933?style=for-the-badge&logo=nodedotjs&logoColor=white" /></a>
+    <a href = "https://pnpm.io/installation" alt = "pnpm">
+        <img src = "https://img.shields.io/badge/pnpm-%234a4a4a.svg?style=for-the-badge&logo=pnpm&logoColor=white"></a>
+    <a href = "https://javascript.com" alt = "javascript">
+        <img src = "https://img.shields.io/badge/JavaScript-323330?style=for-the-badge&logo=javascript&logoColor=white"></a>
+</p>
+
+
+## Installation
+
+- Git Clone or Download this repository.
+
+- Install pnpm & node.js:
+
+```bash
+npm i -g pnpm # if installing pnpm using npm
+```
+
+## Usage
+
+- Open a terminal in the `hi3-fps-unlock` directory.
+
+- If you are using the program for the first time, run `pnpm i`. This will install the required dependencies.
+
+- Run `pnpm start` to run the program, follow the instructions in the CLI.
+
+**Note: You do NOT have to run this as administrator.**
+
+## Contributing
+
+Pull requests are welcome.
+
+## License
+
+Licensed under [MIT](https://mit.dromzeh.dev/)
---
+++ index.js
@@ -0,0 +1,88 @@
+// (c) 2023 - drozmeh (Marcel).<marcel@dromzeh.dev>
+// Licensed under the MIT license - https://mit.dromzeh.dev/
+
+// importing the required modules
+const Registry = require('winreg');
+const readline = require('readline');
+const { Buffer } = require('buffer');
+
+// creating a new registry key object for the Hi3 regkey
+const regKey = new Registry({
+  hive: Registry.HKCU,
+  key: '\\Software\\miHoYo\\Honkai Impact 3rd' // The path to the Hi3 regkey.
+});
+
+// checking if the Hi3 regkey exists
+regKey.keyExists(function(err, exists) {
+  if (err) {
+    console.log(err);
+  } else {
+    if (exists) {
+      console.log('Found Hi3 regkey, checking for PersonalGraphicsSetting subkey...');
+
+      // checking if the PersonalGraphicsSetting subkey exists
+      regKey.values(function(err, items) {
+        if (err) {
+          console.log(err);
+        } else {
+          const value = items.find(item => item.type === 'REG_BINARY' && item.name.includes('GENERAL_DATA_V2_PersonalGraphicsSettingV2'));
+          if (value) {
+            // Found the binary value, now we can start parsing the data
+            console.log(`Found Graphics Settings Binary value with name '${value.name}'\n`);
+
+            // Convert the hex data to a string
+            const hexData = value.value.toString('hex');
+            const buffer = Buffer.from(hexData, 'hex');
+            const dataString = buffer.toString();
+
+            //console.log(`The data in the value is: ${dataString}`);
+
+            // Clean the data string to remove all non-printable characters, then parse it to JSON
+            const printableAscii = /[\x20-\x7E]+/g;
+            const cleanDataString = dataString.match(printableAscii).join('');
+            const graphicsSettings = JSON.parse(cleanDataString);
+
+            // Print the current FPS settings
+            console.log(`Current FPS out of combat: ${graphicsSettings.TargetFrameRateForOthers}`);
+            console.log(`Current FPS in combat: ${graphicsSettings.TargetFrameRateForInLevel}\n`);
+
+            // Ask the user for the desired FPS settings
+            const rl = readline.createInterface({
+              input: process.stdin,
+              output: process.stdout
+            });
+
+            rl.question('Enter the desired FPS in combat: ', (combatFPS) => {
+              rl.question('Enter the desired FPS outside of combat: ', (outOfCombatFPS) => {
+                graphicsSettings.TargetFrameRateForInLevel = parseInt(combatFPS);
+                graphicsSettings.TargetFrameRateForOthers = parseInt(outOfCombatFPS);
+
+                console.log(`\nThe modified graphics settings are: ${JSON.stringify(graphicsSettings)}\n`);
+
+                // Converts the modified object back to hex
+                const modifiedDataString = JSON.stringify(graphicsSettings);
+                const modifiedBuffer = Buffer.from(modifiedDataString);
+                const modifiedHexData = modifiedBuffer.toString('hex');
+
+                // Updating the binary value with the new hex data
+                regKey.set(value.name, Registry.REG_BINARY, modifiedHexData, function(err) {
+                  if (err) {
+                    console.log(err);
+                  } else {
+                    console.log(`Updated value ${value.name} to ${modifiedHexData}\n`);
+                  }
+                });
+
+                rl.close();
+              });
+            });
+          } else {
+            console.log('Did not find value\n');
+          }
+        }
+      });
+    } else {
+      console.log('Hi3 regkey does not exist!\n');
+    }
+  }
+});
