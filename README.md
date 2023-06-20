dc27531dcb3b6b535700bbe8dc476e2624a8df07
Marcel <65048232+dromzeh@users.noreply.github.com>
2023-06-20 06:41:46 +0100

    feat: hsr support

--- README.md
+++ README.md
@@ -1,12 +1,12 @@
-# hi3-fps-unlocker
+# hyv-fps-unlocker
 
-Program that allows you to modify the fps of Hi3 in and outside of levels to any value of your choice.
-Read the blog post about this project [here](https://dromzeh.dev/posts/hi3-fps-unlock/).
+Program that allows you to modify the fps of Honkai Impact 3rd and Honkai: Star Rail on PC.
+
+Read the blog post about this project (For Hi3 unlocking) [here](https://dromzeh.dev/posts/hi3-fps-unlock/).
 
 > **Note**:
 > Because you're modifying the registry, changing your options in-game will overwrite the fps to the value you set in-game. To change the fps again, you will have to re-run the program.
 
-If you're looking for a Honkai: Star Rail unlocker, check out [hsr-fps-unlocker](https://github.com/dromzeh/hsr-fps-unlocker)
 
 > **Warning**:
 > I am not responsible for any consequences that may occur from using this program. Use at your own risk.
@@ -15,7 +15,7 @@ If you're looking for a Honkai: Star Rail unlocker, check out [hsr-fps-unlocker]
 
 ## From the releases page
 
-- Download the latest release, then run `hi3-fps-unlocker.exe`
+- Download the latest release, then run `hyv-fps-unlocker.exe`
 
 ## Building
 
--- src/main.rs
+++ src/main.rs
@@ -11,44 +11,77 @@ use winreg::RegKey;
 use winreg::RegValue;
 
 fn main() -> Result<(), Box<dyn Error>> {
+    // ask user which game to update FPS settings for
+    let mut input = String::new();
+    println!("Modify FPS for which game? (hsr/hi3):");
+    io::stdin().read_line(&mut input)?;
+    let game = input.trim().to_lowercase();
+    input.clear();
+
+    // get registry key and value name based on selected game
+    let (reg_key_path, value_name_contains) = match game.as_str() {
+        "hi3" => (
+            "Software\\miHoYo\\Honkai Impact 3rd",
+            "PersonalGraphicsSettingV2",
+        ),
+        "hsr" => ("Software\\Cognosphere\\Star Rail", "GraphicsSettings_Model"),
+        _ => {
+            println!("Invalid game selection");
+            return Ok(());
+        }
+    };
+
     // get raw_value from registry
     let hkcu = RegKey::predef(HKEY_CURRENT_USER);
-    let hi3_regkey =
-        hkcu.open_subkey_with_flags("Software\\miHoYo\\Honkai Impact 3rd", KEY_ALL_ACCESS)?;
-    let values = hi3_regkey
+    let reg_key = hkcu.open_subkey_with_flags(reg_key_path, KEY_ALL_ACCESS)?;
+    let values = reg_key
         .enum_values()
         .map(|x| x.unwrap().0)
         .collect::<Vec<_>>();
     let value_name = values
         .iter()
-        .find(|&x| x.contains("PersonalGraphicsSettingV2"))
+        .find(|&x| x.contains(value_name_contains))
         .unwrap();
-    println!("Found PersonalGraphicsSettingV2 at {:?}", value_name);
-    let raw_value = hi3_regkey.get_raw_value(value_name)?;
-
-    // println!("{:?}", raw_value.bytes);
+    println!("Found {} at {:?}", value_name_contains, value_name);
+    let raw_value = reg_key.get_raw_value(value_name)?;
 
     // parse raw_value binary data into json, then print it
-    let json_value: Value = serde_json::from_slice(&raw_value.bytes)?;
-    // println!("{:?}", json_value);
+    let mut json_value: Value = serde_json::from_slice(&raw_value.bytes)?;
 
-    // ask user for input, to modify TargetFrameRateForInLevel & TargetFrameRateForOthers
-    let mut input = String::new();
-    println!("Enter desired FPS in level: ");
-    io::stdin().read_line(&mut input)?;
-    let target_frame_rate_for_in_level: u32 = input.trim().parse()?;
-    input.clear();
+    // ask user for input, to modify FPS
+
+    // print json_value
     println!("");
-    println!("Enter desired FPS out of level: ");
-    io::stdin().read_line(&mut input)?;
-    let target_frame_rate_for_others: u32 = input.trim().parse()?;
-    input.clear();
+    println!("Current FPS settings for {}", game);
+    if game == "hsr" {
+        println!("FPS: {}", json_value["FPS"]);
+    } else {
+        println!("In Level: {}", json_value["TargetFrameRateForInLevel"]);
+        println!("Out of Level: {}", json_value["TargetFrameRateForOthers"]);
+    }
+    println!("");
 
-    // modify json_value
-    let mut json_value = json_value;
-    json_value["TargetFrameRateForInLevel"] = json!(target_frame_rate_for_in_level);
-    json_value["TargetFrameRateForOthers"] = json!(target_frame_rate_for_others);
-    // println!("{:?}", json_value);
+    if game == "hsr" {
+        println!("Enter desired FPS (30/60/120): ");
+        io::stdin().read_line(&mut input)?;
+        let fps: u32 = input.trim().parse()?;
+        input.clear();
+
+        json_value["FPS"] = json!(fps);
+    } else {
+        println!("Enter desired FPS in level: ");
+        io::stdin().read_line(&mut input)?;
+        let target_frame_rate_for_in_level: u32 = input.trim().parse()?;
+        input.clear();
+        println!("");
+        println!("Enter desired FPS out of level: ");
+        io::stdin().read_line(&mut input)?;
+        let target_frame_rate_for_others: u32 = input.trim().parse()?;
+        input.clear();
+
+        json_value["TargetFrameRateForInLevel"] = json!(target_frame_rate_for_in_level);
+        json_value["TargetFrameRateForOthers"] = json!(target_frame_rate_for_others);
+    }
 
     // convert json_value back to binary data
     let new_raw_value = RegValue {
@@ -57,20 +90,12 @@ fn main() -> Result<(), Box<dyn Error>> {
     };
 
     // write new_raw_value to registry
-    hi3_regkey.set_raw_value(value_name, &new_raw_value)?;
-
-    // print new_raw_value
-    // let new_raw_value = hi3_regkey.get_raw_value(value_name)?;
-    // println!("{:?}", new_raw_value.bytes);
+    reg_key.set_raw_value(value_name, &new_raw_value)?;
 
     println!("");
-    println!(
-        "Updated FPS settings to {} in level, {} out of level",
-        target_frame_rate_for_in_level, target_frame_rate_for_others
-    );
+    println!("Updated FPS settings for {}", game);
     println!("Press any key to exit");
 
-    let mut input = String::new();
     io::stdin().read_line(&mut input)?;
     Ok(())
 }
